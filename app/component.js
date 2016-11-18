3b49e98e7f89c18aa8a35f3a615fff5e989cdcb2
missalotof <missalotof@163.com>
2016-11-18 21:29:26 +0800

    webpack-demo

---
+++ app/component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+module.exports = function () {
+    console.log("1111111111111111");
+    var element = document.createElement('h1');
+
+    element.innerHTML = "hello world!";
+
+    return element;
+};
---
+++ app/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+console.log("3333333333333");
+
+var component = require('./component.js');
+console.log("2222222222222222222");
+
+
+document.body.appendChild(component());
